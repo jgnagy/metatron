fbe53d2d1290643f6892cff6bc9789c6852879b9
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2023-08-22 16:28:17 -0700

    feat: add etag support

--- lib/metatron/sync_controller.rb
+++ lib/metatron/sync_controller.rb
@@ -9,9 +9,21 @@ module Metatron
     end
 
     post "/" do
+      # TODO: move this to Sinatra's `etag` helper when Metacontroller is RFC compliant
+      if (provided_etag = calculate_etag) &&
+         (match_header = request.env["HTTP_IF_NONE_MATCH"]) &&
+         match_header.split(/,\s?/).include?("\"#{provided_etag}\"")
+        halt 304
+      end
+
+      # If the etag is available, use it, otherwise proceed with the sync
+      headers "ETag" => "\"#{provided_etag}\"" if provided_etag
       data = sync
       data[:children] = data[:children]&.map { |c| c.respond_to?(:render) ? c.render : c }
       halt(data.to_json)
     end
+
+    def calculate_etag = nil
+    def sync = raise NotImplementedError
   end
 end
