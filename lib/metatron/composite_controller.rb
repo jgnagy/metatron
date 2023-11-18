b5e30fd1da73a295a11e6c71172b6e52c211abe9
Jonathan Gnagy <jonathan@therubyist.org>
2023-11-17 22:59:21 -0700

    feat: adding finalize and customize to composite
    Also bumping up the RSpec/ExampleLength to 10 lines

--- lib/metatron/composite_controller.rb
+++ lib/metatron/composite_controller.rb
@@ -10,7 +10,7 @@ module Metatron
     end
 
     post "/sync" do
-      if (provided_etag = calculate_etag)
+      if (provided_etag = calculate_sync_etag)
         etag provided_etag
       end
 
@@ -19,7 +19,36 @@ module Metatron
       halt(data.to_json)
     end
 
-    def calculate_etag = nil
+    options "/finalize" do
+      headers "Access-Control-Allow-Methods" => ["POST"]
+      halt 200
+    end
+
+    post "/finalize" do
+      # finalize calls should be rare and unique enough that we don't need to worry about ETags
+
+      data = finalize
+      data[:children] = data[:children]&.map { |c| c.respond_to?(:render) ? c.render : c }
+      halt(data.to_json)
+    end
+
+    options "/customize" do
+      headers "Access-Control-Allow-Methods" => ["POST"]
+      halt 200
+    end
+
+    post "/customize" do
+      if (provided_etag = calculate_customize_etag)
+        etag provided_etag
+      end
+
+      halt(customize.to_json)
+    end
+
+    def calculate_customize_etag = nil
+    def calculate_sync_etag = nil
+    def customize = raise NotImplementedError
+    def finalize = raise NotImplementedError
     def sync = raise NotImplementedError
   end
 end
--- spec/metatron/composite_controller_spec.rb
+++ spec/metatron/composite_controller_spec.rb
@@ -4,11 +4,16 @@ require "rack/test"
 
 class TestController < Metatron::CompositeController
   def sync = { status: {}, children: [] }
+  def finalize = { status: {}, children: [], finalized: true }
+  def customize = { relatedResources: [] }
 end
 
 class TestControllerWithEtags < Metatron::CompositeController
-  def calculate_etag = "abcd1234"
+  def calculate_sync_etag = "abcd1234"
+  def calculate_customize_etag = "efgh5678"
   def sync = { status: {}, children: [] }
+  def finalize = { status: {}, children: [], finalized: true }
+  def customize = { relatedResources: [] }
 end
 
 RSpec.describe Metatron::CompositeController do
@@ -32,7 +37,7 @@ RSpec.describe Metatron::CompositeController do
       expect(last_response.headers.keys).not_to include("ETag")
     end
 
-    it "returns a 200 for standard sync requests" do # rubocop:disable RSpec/ExampleLength
+    it "returns a 200 for standard sync requests" do
       post(
         "/sync",
         {},
@@ -45,7 +50,7 @@ RSpec.describe Metatron::CompositeController do
       expect(last_response.status).to eq(200)
     end
 
-    it "provides the expected data for standard sync requests" do # rubocop:disable RSpec/ExampleLength
+    it "provides the expected data for standard sync requests" do
       post(
         "/sync",
         {},
@@ -56,6 +61,92 @@ RSpec.describe Metatron::CompositeController do
         }
       )
       expect(last_response.body).to eq({ status: {}, children: [] }.to_json)
+    end
+
+    it "returns a 200 OK for finalize requests" do
+      post(
+        "/finalize",
+        { finalizing: true }.to_json,
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the expected data for finalize requests" do
+      post(
+        "/finalize",
+        { finalizing: true }.to_json,
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.body).to eq({ status: {}, children: [], finalized: true }.to_json)
+    end
+
+    it "returns a 200 OK for initial customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the expected data for initial customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.body).to eq({ relatedResources: [] }.to_json)
+    end
+
+    it "does not provide an ETag header for initial customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.headers.keys).not_to include("ETag")
+    end
+
+    it "returns a 200 for standard customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "HTTP_IF_NONE_MATCH" => "\"efgh5678\"", # This should be ignored
+          "CONTENT_TYPE" => "application/json"
+        }
+      )
+      expect(last_response.status).to eq(200)
+    end
+
+    it "provides the expected data for standard customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "HTTP_IF_NONE_MATCH" => "\"efgh5678\"", # This should be ignored
+          "CONTENT_TYPE" => "application/json"
+        }
+      )
+      expect(last_response.body).to eq({ relatedResources: [] }.to_json)
     end
   end
 
@@ -77,7 +168,7 @@ RSpec.describe Metatron::CompositeController do
       expect(last_response.headers).to include("ETag" => "\"abcd1234\"")
     end
 
-    it "returns a 412 for standard sync requests with ETag headers" do # rubocop:disable RSpec/ExampleLength
+    it "returns a 412 for standard sync requests with ETag headers" do
       post(
         "/sync",
         {},
@@ -90,13 +181,75 @@ RSpec.describe Metatron::CompositeController do
       expect(last_response.status).to eq(412)
     end
 
-    it "does not provide a body for standard sync requests with ETag headers" do # rubocop:disable RSpec/ExampleLength
+    it "does not provide a body for standard sync requests with ETag headers" do
       post(
         "/sync",
         {},
         {
           "HTTP_ACCEPT" => "application/json",
           "HTTP_IF_NONE_MATCH" => "\"abcd1234\"",
+          "CONTENT_TYPE" => "application/json"
+        }
+      )
+      expect(last_response.body).to be_empty
+    end
+
+    it "returns a 200 OK for initial customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns the expected data for initial customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.body).to eq({ relatedResources: [] }.to_json)
+    end
+
+    it "returns the expected ETag header for initial customize requests" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "Content-Type" => "application/json"
+        }
+      )
+      expect(last_response.headers).to include("ETag" => "\"efgh5678\"")
+    end
+
+    it "returns a 412 for standard customize requests with ETag headers" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "HTTP_IF_NONE_MATCH" => "\"efgh5678\"",
+          "CONTENT_TYPE" => "application/json"
+        }
+      )
+      expect(last_response.status).to eq(412)
+    end
+
+    it "does not provide a body for standard customize requests with ETag headers" do
+      post(
+        "/customize",
+        {},
+        {
+          "HTTP_ACCEPT" => "application/json",
+          "HTTP_IF_NONE_MATCH" => "\"efgh5678\"",
           "CONTENT_TYPE" => "application/json"
         }
       )
