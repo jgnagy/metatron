55315cf0b7c200754961ae1f3d13f33e3256e792
Hartley McGuire <skipkayhil@gmail.com>
2023-11-21 16:14:49 -0500

    Rackify Controller and CompositeController
    This removes Sinatra as the underlying class of Controller and
    CompositeController and replaces features previously implemented by
    Sinatra with plain Rack.
    
    The most notable feature kept is ETags/Conditional Get which is kept
    using the If-None-Match header.
    
    An additional thing to note is that Controller is implemented slightly
    differently than PingController. PingController does not really carry
    any state so it's implemented as a persistent instance that can be
    called. Metatron::Controller does want to carry some state that can be
    used in the implementing class's methods, so intead of a single instance
    the Controller will be instantiated per request when called.

--- lib/metatron/controller.rb
+++ lib/metatron/controller.rb
@@ -2,37 +2,37 @@
 
 module Metatron
   # Base class for API services
-  class Controller < Sinatra::Base
-    helpers Sinatra::CustomLogger
-
-    configure do
-      set :protection, except: :http_origin
-      set :logging, true
-      set :logger, Metatron.logger
-      set :show_exceptions, false
-    end
-
-    before do
-      # Sets up a useful variable (@json_body) for accessing a parsed request body
-      if request.content_type&.include?("json") && !request.body.read.empty?
-        request.body.rewind
-        @json_body = JSON.parse(request.body.read)
+  class Controller
+    class << self
+      def call(env)
+        new(env).call
       end
-    rescue StandardError => e
-      halt(400, { error: "Request must be JSON: #{e.message}}" }.to_json)
     end
 
-    error do
-      content_type :json
+    attr_accessor :request_body
 
-      e = env["sinatra.error"]
-      resp = { result: "error", message: e.message }
-      resp[:trace] = e.full_message if settings.environment.to_s != "production"
-      resp.to_json
+    def initialize(env)
+      @env = env
+      @request = Rack::Request.new(env)
     end
 
-    def request_body
-      @json_body
+    def call
+      begin
+        if request&.content_type&.include?("json")
+          body = request.body.read
+          request.body.rewind if request.body.respond_to?(:rewind)
+
+          self.request_body = JSON.parse(body) unless body.empty?
+        end
+      rescue JSON::ParserError => e
+        return [400, {}, [{ error: "Request must be JSON: #{e.message}" }.to_json]]
+      end
+
+      _call
     end
+
+    private
+
+    attr_reader :request
   end
 end
