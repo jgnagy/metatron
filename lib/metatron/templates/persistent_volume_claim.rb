5339a4ea732695530814e281f6d0ae2de3e7889d
Jonathan Gnagy <jonathan@therubyist.org>
2025-02-11 21:49:13 -0700

    feat: adds dataSource and dataSourceRef to PVCs

--- lib/metatron/templates/persistent_volume_claim.rb
+++ lib/metatron/templates/persistent_volume_claim.rb
@@ -7,10 +7,13 @@ module Metatron
       include Concerns::Annotated
       include Concerns::Namespaced
 
-      attr_accessor :additional_labels, :storage_class, :access_modes, :storage
+      attr_accessor :additional_labels, :data_source, :data_source_ref, :storage_class,
+                    :access_modes, :storage
 
       alias storageClassName storage_class
       alias accessModes access_modes
+      alias dataSource data_source
+      alias dataSourceRef data_source_ref
 
       def initialize(
         name,
@@ -22,6 +25,7 @@ module Metatron
         @storage_class = storage_class
         @access_modes = access_modes
         @storage = storage
+        @data_source = {}
         @additional_labels = {}
       end
 
@@ -41,8 +45,32 @@ module Metatron
                 storage:
               }
             }
-          }
+          }.merge(formatted_data_source).merge(formatted_data_source_ref)
         }
+      end
+
+      def formatted_data_source
+        return {} unless data_source && !data_source.empty?
+        raise "dataSource must be a Hash" unless data_source.is_a?(Hash)
+
+        valid_keys = %i[apiGroup kind name]
+        unless data_source.keys.all? { valid_keys.include?(_1.to_sym) }
+          raise "dataSource may contain only the keys: #{valid_keys.join(", ")}"
+        end
+
+        { dataSource: data_source.transform_keys(&:to_sym) }
+      end
+
+      def formatted_data_source_ref
+        return {} unless data_source_ref && !data_source_ref.empty?
+        raise "dataSourceRef must be a Hash" unless data_source_ref.is_a?(Hash)
+
+        valid_keys = %i[apiGroup kind name namespace]
+        unless data_source_ref.keys.all? { valid_keys.include?(_1.to_sym) }
+          raise "dataSourceRef may contain only the keys: #{valid_keys.join(", ")}"
+        end
+
+        { dataSourceRef: data_source_ref.transform_keys(&:to_sym) }
       end
     end
   end
